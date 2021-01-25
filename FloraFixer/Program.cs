cb830fb19f2cc36e955c6abb265be2a04b9313c7
Minizbot2012 <mini@zbots.org>
2021-01-25 17:43:09 -0500

    Clean Code, muta/syn update

--- FloraFixer/Program.cs
+++ FloraFixer/Program.cs
@@ -10,33 +10,40 @@ namespace SynFloraFixer
     {
         public static async Task<int> Main(string[] args)
         {
-            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences() {
-                    ActionsForEmptyArgs = new RunDefaultPatcher
-                    {
-                        IdentifyingModKey = "FloraFixer.esp",
-                        TargetRelease = GameRelease.SkyrimSE
-                    }
-                });
+            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences()
+            {
+                ActionsForEmptyArgs = new RunDefaultPatcher
+                {
+                    IdentifyingModKey = "FloraFixer.esp",
+                    TargetRelease = GameRelease.SkyrimSE
+                }
+            });
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            foreach(var tree in state.LoadOrder.PriorityOrder.OnlyEnabled().Tree().WinningOverrides()) {
-                if(tree.VirtualMachineAdapter == null) {
+            foreach (var tree in state.LoadOrder.PriorityOrder.OnlyEnabled().Tree().WinningOverrides())
+            {
+                if (tree.VirtualMachineAdapter == null)
+                {
                     var otree = state.PatchMod.Trees.GetOrAddAsOverride(tree);
                     Console.WriteLine($"Patching TREE {otree.EditorID}");
                     otree.VirtualMachineAdapter = new VirtualMachineAdapter();
-                    otree.VirtualMachineAdapter.Scripts.Add(new ScriptEntry() {
+                    otree.VirtualMachineAdapter.Scripts.Add(new ScriptEntry()
+                    {
                         Name = "florafix",
                     });
                 }
             }
-            foreach(var flora in state.LoadOrder.PriorityOrder.OnlyEnabled().Flora().WinningOverrides()) {
-                if(flora.VirtualMachineAdapter == null) {
-                    var otree = state.PatchMod.Florae.GetOrAddAsOverride(flora);;
+            foreach (var flora in state.LoadOrder.PriorityOrder.OnlyEnabled().Flora().WinningOverrides())
+            {
+                if (flora.VirtualMachineAdapter == null)
+                {
+                    var otree = state.PatchMod.Florae.GetOrAddAsOverride(flora); ;
                     Console.WriteLine($"Patching FLOR {otree.EditorID}");
                     otree.VirtualMachineAdapter = new VirtualMachineAdapter();
-                    otree.VirtualMachineAdapter.Scripts.Add(new ScriptEntry() {
+                    otree.VirtualMachineAdapter.Scripts.Add(new ScriptEntry()
+                    {
                         Name = "florafix",
                     });
                 }
