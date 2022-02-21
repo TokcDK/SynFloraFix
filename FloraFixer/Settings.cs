f8fb4972d8aa0fadd2bd7a3f759d8712b3213e0e
Denis K <tokcdk@gmail.com>
2022-02-21 17:06:51 +0500

    added setting to set flora respawn fix script name added fix for missing activate text string added fix for zero production chance tweak for script record create only one time and set it instead of creating each time mutagen lib up to 0.36.1-pr001

---
+++ FloraFixer/Settings.cs
@@ -0,0 +1,10 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace FloraFixer
+{
+    public class Settings
+    {
+        [SettingName("Name of Flora Respawn fix script ('florafix' by default)")]
+        public string ScriptName = "florafix";
+    }
+}
